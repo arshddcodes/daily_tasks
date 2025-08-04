85d117c662d687a746bb9f90133b19c76d68e680
arshddcodes <arshadkallankodan@gmail.com>
2025-08-04 19:58:49 +0530

    day 11 added

--- day10/for-task.js
+++ day10/for-task.js
@@ -25,14 +25,56 @@
 //     }
 // }
 
+// for (let number = 1; number <= 10; number++){
+//     let div = number % 2;
+
+//     if (div !== 0) {
+//         console.log(number)
+
+//     }
+
+// }
+
+// prime number
+
+// for (let i= 2; i <= 10; i++){
+//     let prime = true;
+//     for (let j = 2; j  < i; j++) {
+//     if (i % j === 0) {
+//       prime = false;
+//        break;
+//     }
+//   }
+
+//   if(prime){
+//     console.log(i);
+//   }
+
+// }
+
+// for (let row = 0; row <= 4; row++) {
+//   let output = "";
+//   for (let j = 0; j <= 4; j++) {
+//     output += "*";
+//   }
+//   console.log(output);
+// }
+
+// for (let row = 0; row <= 4; row++) {
+//   let output = "";
+//   for (let j = 0; j <= row; j++) {
+//     output += "*";
+//   }
+//   console.log(output);
+// }
 
 
-for (let number = 1; number <= 10; number++){
-    let div = number % 2;
-
-    if (div !== 0) {
-        console.log(number)
-
-    }
-    
+for (let i = 4; i >= 0; i--) {
+  let output = "";
+  for (let j = 0; j <= i; j++) {
+    output += "*";
+  }
+  console.log(output);
 }
+
+
---
+++ day11/esSix.js
@@ -0,0 +1,13 @@
+//spread
+let arr = [1, 2, 3, 4];
+let arrTwo = [...arr, 5, 6];
+
+// destructuring
+let de = [3, 3, "arshad"];
+let [one, two, name] = de;
+
+//Arrow fun
+const arrfunction = () => {};
+
+//map
+data.map(() => {});
