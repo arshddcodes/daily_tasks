5a97ab23d157c28f78b6b87577c4a7ca6f45201e
arshddcodes <arshadkallankodan@gmail.com>
2025-08-11 17:53:18 +0530

    day12 updated

--- day12/react-app/vite-project/src/main.jsx
+++ day12/react-app/vite-project/src/main.jsx
@@ -3,8 +3,83 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+const name = "arshad";
+const num = 9;
+const img ="https://picsum.photos/200/300";
+
+// const colorStyle ={
+//   color: "green",
+// }
+// const blue ={
+//   color: "blue",
+// }
+
+// const green ={
+//   color: "green",
+// }
+
+// const yellow ={
+//   color: "yellow",
+// }
+
+
+const headline = 
+
+function listStyles(){
+  return(
+    <div>
+      <h1>My Favourite Movies</h1>
+      <ul>
+        <li>maranamass</li>
+        <li>kunjiramayanam</li>
+        <li>Bangalore Days</li>
+      </ul>
+    </div>
+  );
+}
+
+// colorStyle.color = "orange"
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <div className='text'>
+    <listStyles/>
+  </div>
+);
+
+//  <div className='text'>
+
+//     <h1 style={colorStyle}>My Favourite Movies</h1>
+//     <ul>
+//       <li style = {blue}>maranamass</li>
+//       <li style = {green}>malik</li>
+//       <li style = {yellow}>bangalore days</li>
+//     </ul>
+//     <img src= {img}  alt=""/>
+
+//   </div>
+
+ {/* <h1>My Favourite Movies</h1>
+    <ul>
+      <li style={{
+        color:"blue",
+      }}>maranamass</li>
+      <li style={{
+        color: "green",
+      }}>malik</li>
+      <li style={{
+        color:"yellow",
+      }}>bangalore days</li>
+    </ul>
+    <img src= {img}  alt=""/> */}
+
+
+ // first task
+  // <div> <h1>Hello {name} !</h1>
+  // <h1>my number is {num}</h1
+  // >
+  // </div>
+
+
+// {/* <StrictMode>
+//     <App />
+//   </StrictMode>,/ */}
